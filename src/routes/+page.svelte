e41b68e127f5a10e5e9ad2615de834ed3c2fa762
rchrdnsh <rchrdnsh@gmail.com>
2022-12-13 09:41:37 -0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<!-- uncomment this h1 to see tha app working fine -->
+<!-- <h1>Hi</h1> -->
+
+
+<!-- comment all this out to see the app work just fine with an h1 -->
+<script>
+  import { Motion } from 'svelte-motion'
+</script>
+
+<Motion let:motion>
+  <div use:motion/>
+</Motion>
+
+<style>
+  div {
+    width: 256px;
+    height: 256px;
+    background: red;
+  }
+</style>
